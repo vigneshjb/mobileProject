77c9baee12c94b077b397b8470d9a2e5de2553de
Vignesh Jayabalan <vigneshjayabalan@gmail.com>
2015-11-08 00:33:03 -0700

    backend service is done

---
+++ restService/backend.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+require 'json'
+
+get '/healthcheck' do
+  "Hello World!"
+end
+
+post '/logs' do
+	begin 
+		result_set = JSON.parse(request.body.read)['logs']
+	rescue
+		status 400
+		return "Logs not found"
+	end
+
+	if ( result_set == nil )
+		status 400
+		return "Logs not found"
+	else
+		savelogs(Time.now, result_set)
+		status 200
+	end
+
+	result_set.to_s
+end
+
+def savelogs(time, result_set)
+	File.open(time.to_s,"w+") do |f|
+		result_set.each do |result|
+			f.puts(result.to_s)
+		end
+	end
+end
